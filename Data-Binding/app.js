cf68d8a9b27e12905b62c2a1496f3cb7573d467d
Anuteja <anuteja@fissionlabs.in>
2013-10-23 18:12:09 +0530

    Data binding in angular

--- Data-Binding/app.js
+++ Data-Binding/app.js
@@ -1,8 +1,20 @@
 
 var app = angular.module('testApp', []);
 
-function TestCtrl($scope) {
+app.controller('TestCtrl', ['$scope',
+    function($scope){
+        var personDetails = {username : "Sample", password : "password"}
+       $scope.user ={name :"", pwd : ""};
+        $scope.message ="";
 
-    var user = {name :"", pwd : ""};
-    $scope.user =user;
-}
+        $scope.authenticateUser = function(){
+            if(personDetails.username === $scope.user.name && personDetails.password === $scope.user.pwd){
+                $scope.message = "You are authenticated"
+            }else{
+                $scope.message = "Invalid Username or Password.";
+            }
+            $scope.user ={name :"", pwd : ""};
+        };
+
+}]);
+
